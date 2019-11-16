4826fb18181d694891b927c08200b767540cdb61
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2019-11-16 19:01:12 +0300

    Created hello project

---
+++ app/src/main/java/com/eshevtsov/android/crypto/LauncherActivity.kt
@@ -0,0 +1,5 @@
+package com.eshevtsov.android.crypto
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity(R.layout.activity_main)
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name="Crypto"
+
+include(
+    Modules.app
+)
