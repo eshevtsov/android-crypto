8dc68c07fa0e07078f4f033a4b4f7c0e5f42cbdb
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2019-11-20 15:49:11 +0300

    Refactored coins list service

--- data/src/main/java/com/eshevtsov/android/crypto/data/network/CoinListService.kt
+++ data/src/main/java/com/eshevtsov/android/crypto/data/network/CoinListService.kt
@@ -2,9 +2,10 @@ package com.eshevtsov.android.crypto.data.network
 
 import com.eshevtsov.android.crypto.data.dto.CoinListResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CoinListService {
 
-    @GET("cryptocurrency/listings/latest?limit=5000")
-    suspend fun list(): CoinListResponseDto
+    @GET("cryptocurrency/listings/latest")
+    suspend fun list(@Query("limit") limit: Int = 1000): CoinListResponseDto
 }
